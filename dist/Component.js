e946678cb52039a25116e955184538f9b86740b1
imran khan mohammad <imran.endeavour@gmail.com>
2021-09-20 06:21:17 +0000

    first

---
+++ dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","mm/podata/model/models"],function(e,t,i){"use strict";return e.extend("mm.podata.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);this.getRouter().initialize();this.setModel(i.createDeviceModel(),"device")}})});
---
+++ dist/controller/View1.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],function(n){"use strict";return n.extend("mm.podata.controller.View1",{onInit:function(){}})});
